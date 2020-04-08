b6764fd7b2828510446f188f8637cdeb8f59f527
Lorence Lim <jlorencelim@gmail.com>
2020-04-08 07:16:12 +0000

    Initial commit.

---
+++ src/config/app.js
@@ -0,0 +1,4 @@
+export const appConfig = {
+    apiUrl: 'try-me-44.botics.co/api',
+    defaultTimeout: 5000,
+}
---
+++ try_me_44/urls.py
@@ -0,0 +1,53 @@
+"""try_me_44 URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include, re_path
+from django.views.generic import TemplateView
+
+from allauth.account.views import confirm_email
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+
+# Swagger
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Try Me API",
+        default_version="v1",
+        description="API documentation for Try Me App",
+    ),
+    public=True,
+    permission_classes=(permissions.IsAuthenticated,),
+)
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("accounts/", include("allauth.urls")),
+    path("api/v1/", include(("home.api.v1.urls", "home"), namespace="home-v1")),
+    path("api/auth/", include("rest_auth.urls")),
+    # Override email confirm to use allauth's HTML view instead of rest_auth's API view
+    path("api/auth/registration/account-confirm-email/<str:key>/", confirm_email),
+    path("api/auth/registration/", include("rest_auth.registration.urls")),
+    path("api/docs/", schema_view.with_ui("swagger", cache_timeout=0), name="api_docs"),
+    # React views
+    path("", TemplateView.as_view(template_name="index.html")),
+    re_path(r"^(?:.*)/?$", TemplateView.as_view(template_name="index.html")),
+]
+
+admin.site.site_header = "Try Me"
+admin.site.site_title = "Try Me Admin Portal"
+admin.site.index_title = "Try Me Admin"
